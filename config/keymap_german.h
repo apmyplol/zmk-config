e29ae1053fbd2d6c6805f30ded10c15a0d7979ea
afa <bla>
2023-01-22 20:57:10 +0100

    added other rows german config

--- config/keymap_german.h
+++ config/keymap_german.h
@@ -39,7 +39,7 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define DE_CIRC KC_GRV  // ^ (dead)
+#define DE_CIRC GRAVE   // ^ (dead)
 #define DE_1    N1      // 1
 #define DE_2    N2      // 2
 #define DE_3    N3      // 3
@@ -105,28 +105,28 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define DE_DEG  S(DE_CIRC) // °
-#define DE_EXLM S(DE_1)    // !
-#define DE_DQUO S(DE_2)    // "
-#define DE_SECT S(DE_3)    // §
-#define DE_DLR  S(DE_4)    // $
-#define DE_PERC S(DE_5)    // %
-#define DE_AMPR S(DE_6)    // &
-#define DE_SLSH S(DE_7)    // /
-#define DE_LPRN S(DE_8)    // (
-#define DE_RPRN S(DE_9)    // )
-#define DE_EQL  S(DE_0)    // =
-#define DE_QUES S(DE_SS)   // ?
-#define DE_GRV  S(DE_ACUT) // ` (dead)
+// #define DE_DEG  S(DE_CIRC) // °
+// #define DE_EXLM S(DE_1)    // !
+// #define DE_DQUO S(DE_2)    // "
+// #define DE_SECT S(DE_3)    // §
+// #define DE_DLR  S(DE_4)    // $
+// #define DE_PERC S(DE_5)    // %
+// #define DE_AMPR S(DE_6)    // &
+// #define DE_SLSH S(DE_7)    // /
+// #define DE_LPRN S(DE_8)    // (
+// #define DE_RPRN S(DE_9)    // )
+// #define DE_EQL  S(DE_0)    // =
+// #define DE_QUES S(DE_SS)   // ?
+// #define DE_GRV  S(DE_ACUT) // ` (dead)
 // Row 2
-#define DE_ASTR S(DE_PLUS) // *
+// #define DE_ASTR S(DE_PLUS) // *
 // Row 3
-#define DE_QUOT S(DE_HASH) // '
+// #define DE_QUOT S(DE_HASH) // '
 // Row 4
-#define DE_RABK S(DE_LABK) // >
-#define DE_SCLN S(DE_COMM) // ;
-#define DE_COLN S(DE_DOT)  // :
-#define DE_UNDS S(DE_MINS) // _
+// #define DE_RABK S(DE_LABK) // >
+// #define DE_SCLN S(DE_COMM) // ;
+// #define DE_COLN S(DE_DOT)  // :
+// #define DE_UNDS S(DE_MINS) // _
 
 /* AltGr symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
@@ -142,17 +142,17 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define DE_SUP2 ALGR(DE_2)    // ²
-#define DE_SUP3 ALGR(DE_3)    // ³
-#define DE_LCBR ALGR(DE_7)    // {
-#define DE_LBRC ALGR(DE_8)    // [
-#define DE_RBRC ALGR(DE_9)    // ]
-#define DE_RCBR ALGR(DE_0)    // }
-#define DE_BSLS ALGR(DE_SS)   // (backslash)
+// #define DE_SUP2 ALGR(DE_2)    // ²
+// #define DE_SUP3 ALGR(DE_3)    // ³
+// #define DE_LCBR ALGR(DE_7)    // {
+// #define DE_LBRC ALGR(DE_8)    // [
+// #define DE_RBRC ALGR(DE_9)    // ]
+// #define DE_RCBR ALGR(DE_0)    // }
+// #define DE_BSLS ALGR(DE_SS)   // (backslash)
 // Row 2
-#define DE_AT   ALGR(DE_Q)    // @
-#define DE_EURO ALGR(DE_E)    // €
-#define DE_TILD ALGR(DE_PLUS) // ~
+// #define DE_AT   ALGR(DE_Q)    // @
+// #define DE_EURO ALGR(DE_E)    // €
+// #define DE_TILD ALGR(DE_PLUS) // ~
 // Row 4
-#define DE_PIPE ALGR(DE_LABK) // |
-#define DE_MICR ALGR(DE_M)    // µ
+// #define DE_PIPE ALGR(DE_LABK) // |
+// #define DE_MICR ALGR(DE_M)    // µ
