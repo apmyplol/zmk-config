ff0c03da332f91fe1008f10ed394d31b9cb45862
arthur <>
2023-01-22 15:13:17 +0100

    fixed keymap files from qmk for letters

--- config/keymap_german.h
+++ config/keymap_german.h
@@ -16,7 +16,12 @@
 
 #pragma once
 
-#include "keymap.h"
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+#include <dt-bindings/zmk/keys.h>
+
+// #include "keymap.h"
 
 // clang-format off
 
